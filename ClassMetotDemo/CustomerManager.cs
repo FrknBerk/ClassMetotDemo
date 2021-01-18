66477bc6c34c70108b82616f2cec5a7347915bad
FURKAN BERK AKDAŞ <1160505019@ogr.klu.edu.tr>
2021-01-18 17:54:16 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void AddCustomer(Customer customer)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Id:" + customer.Id + " Ad:"+ customer.Name +
+                " Soyad:"+ customer.Surname + " Mail:" +customer.Email+
+                " Şifre:"+customer.Password + " Müşteri Eklendi ");
+            //Console.WriteLine("Ad : " + customer.);
+            //Console.ReadKey();
+        }
+
+        public void ListCustomer(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Id:" + customer.Id + " Ad:" + customer.Name +
+                    " Soyad:" + customer.Surname + " Mail:" + customer.Email +
+                    " Şifre:" + customer.Password + "\n");
+            }
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Id:" + customer.Id + " Ad:" + customer.Name +
+                " Soyad:" + customer.Surname + " Mail:" +customer.Email +
+                " Şifre:" +customer.Password + " Müşteri Silindi");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Furkan Berk";
+            customer1.Surname = "Akdaş";
+            customer1.Email = "deneme@gmail.com";
+            customer1.Password = 1234;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Lorem";
+            customer2.Surname = "Ipsul";
+            customer2.Email = "lorem@hotmail.com";
+            customer2.Password = 4321;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Test";
+            customer3.Surname = "Deneme";
+            customer3.Email = "testdeneme@outlook.com";
+            customer3.Password = 3222;
+
+
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager manager = new CustomerManager();
+
+            manager.AddCustomer(customer1);
+
+            Console.WriteLine("\n\n");
+
+            manager.ListCustomer(customers);
+
+            Console.WriteLine("\n\n");
+
+            manager.DeleteCustomer(customer3);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
